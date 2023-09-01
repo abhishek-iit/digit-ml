fd927f4c32bd52e2c6c82b8794cab1d460aa1f81
abhishek-iit <m22aie228@iitj.ac.in>
2023-09-01 20:51:24 +0530

    Refactored to add predict_and_eval

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,128 +1,88 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
-
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
+def load_data():
+    return datasets.load_digits()
 
-digits = datasets.load_digits()
+def plot_samples(images, labels, title, n_samples=4):
+    _, axes = plt.subplots(nrows=1, ncols=n_samples, figsize=(10, 3))
+    for ax, image, label in zip(axes, images[:n_samples], labels[:n_samples]):
+        ax.set_axis_off()
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+        ax.set_title(f"{title}: {label}")
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
+def preprocess_data(images):
+    n_samples = len(images)
+    return images.reshape((n_samples, -1))
 
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
+def split_data(data, labels, test_size=0.5):
+    return train_test_split(data, labels, test_size=test_size, shuffle=False)
 
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+def split_train_dev_test(X, y, test_size=0.2, dev_size=0.2):
+    X_temp, X_test, y_temp, y_test = train_test_split(X, y, test_size=test_size, shuffle=True)
+    adjusted_dev_size = dev_size / (1 - test_size)
+    X_train, X_dev, y_train, y_dev = train_test_split(X_temp, y_temp, test_size=adjusted_dev_size, shuffle=True)
+    return X_train, X_dev, X_test, y_train, y_dev, y_test
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+def train_model(X_train, y_train, gamma=0.001):
+    clf = svm.SVC(gamma=gamma)
+    clf.fit(X_train, y_train)
+    return clf
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
+def predict(clf, X_test):
+    return clf.predict(X_test)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+def evaluate_model(y_true, y_pred, clf):
+    print(f"Classification report for classifier {clf}:\n"
+          f"{metrics.classification_report(y_true, y_pred)}\n")
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_true, y_pred)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+    plt.show()
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+def predict_and_eval(model, X_test, y_test):
+    # 6. Predict
+    predicted = predict(clf, X_test)
+  
+    # 7. Plot some test samples with predictions
+    plot_samples(X_test.reshape(-1, 8, 8), predicted, title="Prediction")
+  
+    # 8. Evaluate model
+    evaluate_model(y_test, predicted, clf)
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
+    return predicted   
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+def rebuild_classification_report_from_cm(confusion_matrix):
+    y_true = []
+    y_pred = []
+    for gt in range(len(confusion_matrix)):
+        for pred in range(len(confusion_matrix)):
+            y_true += [gt] * confusion_matrix[gt][pred]
+            y_pred += [pred] * confusion_matrix[gt][pred]
+    
+    print("Classification report rebuilt from confusion matrix:\n"
+          f"{metrics.classification_report(y_true, y_pred)}\n")    
+    
+if __name__ == "__main__":
+    # 1. Load data
+    digits = load_data()
+  
+    # 2. Plot some samples
+    plot_samples(digits.images, digits.target, title="Training")
+  
+    # 3. Preprocess data
+    data = preprocess_data(digits.images)
+  
+    # 4. Split data
+    X_train, X_test, y_train, y_test = split_data(data, digits.target)
+  
+    # 5. Train model
+    clf = train_model(X_train, y_train)
+  
+    # 6. Predict
+    predicted = predict_and_eval(clf, X_test, y_test)
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
-
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
-
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
-
-###############################################################################
-# If the results from evaluating a classifier are stored in the form of a
-# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
-# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
-# as follows:
-
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
+    # 9. Rebuild and display classification report from confusion matrix
+    cm = metrics.confusion_matrix(y_test, predicted)
+    rebuild_classification_report_from_cm(cm)
