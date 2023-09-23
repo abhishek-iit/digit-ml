bea5aedb823974e176988b62a7b19026fbdd894c
abhishek-iit <m22aie228@iitj.ac.in>
2023-09-23 14:25:56 +0530

    Quiz answer 3

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,58 +1,43 @@
+import numpy as np
 from sklearn import datasets
-from utils import tune_hparams, split_train_dev_test, train_model, predict_and_eval, rebuild_classification_report_from_cm, param_combinations 
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from skimage.transform import resize
 
-# Data specific to a given problem
-def load_data():
-    return datasets.load_digits()
+# Load data
+digits = datasets.load_digits()
 
-def preprocess_data(images):
-    n_samples = len(images)
-    return images.reshape((n_samples, -1))
+# Resize function
+def resize_images(images, output_shape):
+    return np.array([resize(image, output_shape, mode='reflect', anti_aliasing=True) for image in images])
 
-# 1. Load data
-digits = load_data()
+# Split and train function
+def train_and_evaluate(images, labels, train_size=0.7, dev_size=0.1, test_size=0.2):
+    X_train, X_temp, y_train, y_temp = train_test_split(images, labels, train_size=train_size, random_state=42)
+    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, train_size=dev_size/(dev_size+test_size), random_state=42)
+    
+    # Flatten the images for training
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_dev = X_dev.reshape(X_dev.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
+    
+    # Train a classifier
+    clf = svm.SVC(gamma=0.001)
+    clf.fit(X_train, y_train)
+    
+    # Evaluate
+    train_acc = clf.score(X_train, y_train) * 100
+    dev_acc = clf.score(X_dev, y_dev) * 100
+    test_acc = clf.score(X_test, y_test) * 100
+    
+    return train_acc, dev_acc, test_acc
 
-# 2. Plot some samples
-#plot_samples(digits.images, digits.target, title="Training")
+# Main function
+def main():
+    for size in [(4, 4), (6, 6), (8, 8)]:
+        resized_images = resize_images(digits.images, size)
+        train_acc, dev_acc, test_acc = train_and_evaluate(resized_images, digits.target)
+        print(f"image size: {size[0]}x{size[1]} train_size: 0.7 dev_size: 0.1 test_size: 0.2 train_acc: {train_acc:.1f} dev_acc: {dev_acc:.1f} test_acc: {test_acc:.1f}")
 
-# 3. Preprocess data
-data = preprocess_data(digits.images)
-
-
-gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
-C_ranges = [0.1, 1, 2, 5, 10]
-# Create a combination of all parameters
-list_of_all_param_combinations = param_combinations(gamma_ranges, C_ranges)
-
-# Splitting
-test_sizes = [0.1, 0.2, 0.3]
-dev_sizes = [0.1, 0.2, 0.3]
-
-for test_size in test_sizes:
-    for dev_size in dev_sizes:
-        X_Train, X_Test, Y_Train, Y_Test, X_Dev, Y_Dev = split_train_dev_test(data, digits.target, test_size, dev_size)
-        best_hparams, best_model, best_accuracy_dev = tune_hparams(X_Train, Y_Train, X_Dev, Y_Dev, list_of_all_param_combinations)
-
-        # Prediction and performance on train set
-        best_accuracy_train = predict_and_eval(best_model, X_Train, Y_Train)
-
-        # Prediction and performance on test set
-        best_accuracy_test = predict_and_eval(best_model, X_Test, Y_Test)
-
-        # 7. Output the results
-        print(f"test_size={test_size} dev_size={dev_size} train_size={1 - test_size - dev_size:.1f} train_acc={best_accuracy_train} dev_acc={best_accuracy_dev:.3f} test_acc={best_accuracy_test:.3f}")
-        print(f"best_hparams: {best_hparams}")
-
-# 4. Split data
-# X_Train, X_Test, Y_Train, Y_Test, X_Dev, Y_Dev = split_train_dev_test(data, digits.target, 0.5, 0.1)
-# X_Train, X_Test, Y_Train, Y_Test = split_train_dev_test(data, digits.target, 0.5, 0)
-
-# 5. Train model
-# model = train_model(X_Train, Y_Train)
-
-# 6. Predict & Evaluate
-# predicted = predict_and_eval(model, X_Test, Y_Test)
-
-# 7. Rebuild and display classification report from confusion matrix
-# rebuild_classification_report_from_cm(Y_Test, predicted)
-
+if __name__ == "__main__":
+    main()
