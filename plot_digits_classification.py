500ece8438fe709336257ca2f888ccf8ad4c45a4
abhishek-iit <m22aie228@iitj.ac.in>
2023-10-07 13:55:59 +0530

    Updating test cases

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,43 +1,53 @@
-import numpy as np
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from skimage.transform import resize
+"""
+================================
+Recognizing hand-written digits
+================================
 
-# Load data
-digits = datasets.load_digits()
+This example shows how scikit-learn can be used to recognize images of
+hand-written digits, from 0-9.
 
-# Resize function
-def resize_images(images, output_shape):
-    return np.array([resize(image, output_shape, mode='reflect', anti_aliasing=True) for image in images])
+"""
 
-# Split and train function
-def train_and_evaluate(images, labels, train_size=0.7, dev_size=0.1, test_size=0.2):
-    X_train, X_temp, y_train, y_temp = train_test_split(images, labels, train_size=train_size, random_state=42)
-    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, train_size=dev_size/(dev_size+test_size), random_state=42)
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Import datasets, classifiers and performance metrics
+from sklearn import metrics, svm
+
+from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
+from joblib import dump, load
+# 1. Get the dataset
+X, y = read_digits()
+
+# 2. Hyperparameter combinations
+# 2.1. SVM
+gamma_list = [0.001, 0.01, 0.1, 1]
+C_list = [1, 10, 100, 1000]
+h_params={}
+h_params['gamma'] = gamma_list
+h_params['C'] = C_list
+h_params_combinations = get_hyperparameter_combinations(h_params)
+
+test_sizes =  [0.1, 0.2, 0.3, 0.45]
+dev_sizes  =  [0.1, 0.2, 0.3, 0.45]
+for test_size in test_sizes:
+    for dev_size in dev_sizes:
+        train_size = 1- test_size - dev_size
+        # 3. Data splitting -- to create train and test sets                
+        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+        # 4. Data preprocessing
+        X_train = preprocess_data(X_train)
+        X_test = preprocess_data(X_test)
+        X_dev = preprocess_data(X_dev)
     
-    # Flatten the images for training
-    X_train = X_train.reshape(X_train.shape[0], -1)
-    X_dev = X_dev.reshape(X_dev.shape[0], -1)
-    X_test = X_test.reshape(X_test.shape[0], -1)
+        best_hparams, best_model_path, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
+        y_dev, h_params_combinations)        
     
-    # Train a classifier
-    clf = svm.SVC(gamma=0.001)
-    clf.fit(X_train, y_train)
-    
-    # Evaluate
-    train_acc = clf.score(X_train, y_train) * 100
-    dev_acc = clf.score(X_dev, y_dev) * 100
-    test_acc = clf.score(X_test, y_test) * 100
-    
-    return train_acc, dev_acc, test_acc
+        # loading of model         
+        best_model = load(best_model_path) 
 
-# Main function
-def main():
-    for size in [(4, 4), (6, 6), (8, 8)]:
-        resized_images = resize_images(digits.images, size)
-        train_acc, dev_acc, test_acc = train_and_evaluate(resized_images, digits.target)
-        print(f"image size: {size[0]}x{size[1]} train_size: 0.7 dev_size: 0.1 test_size: 0.2 train_acc: {train_acc:.1f} dev_acc: {dev_acc:.1f} test_acc: {test_acc:.1f}")
+        test_acc = predict_and_eval(best_model, X_test, y_test)
+        train_acc = predict_and_eval(best_model, X_train, y_train)
+        dev_acc = best_accuracy
 
-if __name__ == "__main__":
-    main()
+        print("test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
